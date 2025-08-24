d523f76c3da1a4a472c00e4d1ea7a5d5341817cc
William <Lavamonster888@gmail.com>
2025-08-24 15:10:32 +1000

    1.7

--- Red/src/main.cpp
+++ Red/src/main.cpp
@@ -2,10 +2,7 @@
 #include "robot-config.h"
 #include "lemlib/api.hpp"
 #include "lemlib/asset.hpp" //Only needed if you use ASSET() paths
-
-
 //NOTES
-
 //Measure distance between tracking wheels and tracking centre
 //Tracking centre is the middle of the drivetrain
 //Find the input in robot-config.cpp, search for "lemlib::trackingwheel horizontal" and "lemlib::trackingwheel vertical"
@@ -66,28 +63,96 @@ static inline bool isBallInIntake() {
 static inline void setIntake(int v) {
     intakeF.move(v); intakeM.move(v); intakeB.move(v);
 }
-void intakeBall() {
-    //Spin intake, drive to a ball 24" ahead, stop when detected
-    setIntake(127);
-    lemlib::Pose target(0, 24);
-    chassis.moveToPoint(target.x, target.y, 1500, {.minSpeed=48});
-    while (chassis.isInMotion() && !isBallInIntake()) pros::delay(10);
-    chassis.cancelMotion(); // in case we detected early
-    setIntake(0);
-}
-ASSET(pathjerryio_txt);
+ASSET(Part1_txt);
+ASSET(Part2_txt);
+ASSET(Part3_txt);
+ASSET(Part4_txt);
+ASSET(Part5_txt);
+ASSET(Part6_txt);
+ASSET(Part7_txt);
+ASSET(Part8_txt);
+ASSET(Part9_txt);
+ASSET(Part10_txt);
 void autonomous() {
     chassis.setPose(0, 0, 0);
 
     //If you want to follow the embedded path from static/example.txt:
     //Lookahead 15", 2s timeout
-    chassis.follow(pathjerryio_txt, 15, 1000);
+    intake();
+    chassis.follow(Part1_txt, 15, 10);
+    intakestop();
+    chassis.follow(Part2_txt, 15, 10);
+    //Spin the intake
+    loading();
+    //The delay time in milisecconds
+    pros::delay(500);
+    //Stop the intake
+    loadingstop();
+    chassis.follow(Part3_txt, 15, 10);
+    chassis.turnToHeading(float (90), int (10));
+    chassis.follow(Part4_txt, 15, 10);
+    //Put blocks into highgoal
+    highgoal();
+    //The delay time in milisecconds
+    pros::delay(500);
+    //Stop putting blocks into the highgoal
+    highgoalstop();
+    chassis.follow(Part5_txt, 15, 10);
+    chassis.turnToHeading(float (0), int (10));
+    chassis.follow(Part6_txt, 15, 10);
+    highgoal();
+    pros::delay(5000);
+    highgoalstop();
+    chassis.follow(Part7_txt, 15, 10);
+    chassis.turnToHeading(float (270), int (10));
+    chassis.follow(Part8_txt, 15, 10);
+    loading();
+    pros::delay(5000);
+    loadingstop();
 
     //Or just do a simple motion:
     //Chassis.moveToPoint(10, 10, 4000);
     //Chassis.moveToPose(10, 10, 90, 4000);
+    chassis.follow(Part9_txt, 15, 10);
+    intake();
+    chassis.follow(Part10_txt, 15, 10);
+    intakestop();
+    lowgoal();
+    pros::delay(5000);
+    lowgoalstop();
 }
+void intake(){
 
+
+}
+void intakestop(){
+
+
+}
+void loading(){
+
+
+}
+void loadingstop(){
+
+
+}
+void lowgoal(){
+
+
+}
+void lowgoalstop(){
+
+
+}
+void highgoal(){
+
+
+}
+void highgoalstop(){
+
+
+}
 void opcontrol() {
     while (true) {
     const double fwd = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) / 127.0;
